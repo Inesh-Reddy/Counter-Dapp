e2634572208969d6a126ee991ccc8546add304b8
Inesh-Reddy <ineshreddy007@gmail.com>
2024-12-24 14:50:20 +0530

    Implemented Counter Dapp

---
+++ src/Counter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+contract Counter { 
+    uint256 num;
+    constructor(uint _num){
+        num = _num;
+    }
+
+    function increment()public{
+        num++;
+    }
+
+    function decrement() public {
+        num--;
+    }
+
+    function getNum() public view returns(uint) {
+        return num;
+    }
+}
---
+++ test/Counter.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/Counter.sol";
+
+contract TestCounter is Test {
+    Counter c;
+
+    function setUp() public {
+        c = new Counter(5);
+    }
+
+    function testIncrement() public {
+        c.increment();
+        c.increment();
+        assertEq(c.getNum(), 7, "ok");
+    }
+
+    function testDecrement() public{
+        c.decrement();
+        assertEq(c.getNum(), 4, "ok");
+    }
+
+}
